826a403ba4a1b0fc9828405839d9591b377031c8
krzysiek02 <k.snopkiewicz02@gmail.com>
2025-04-18 19:44:18 +0200

    version 0.7 Split

--- src/PrzebiegGry.java
+++ src/PrzebiegGry.java
@@ -334,7 +334,6 @@ public final class PrzebiegGry {
             ruchKrupiera();
         }
     }
-
     public void ruchKrupiera() {
         ImageIcon przeskalowanaKarta = ObslugaZdjec.dodawanieZdjecia(kartaDoOdkrycia,talia);
         JLabel kartaLabel = new JLabel(przeskalowanaKarta);
